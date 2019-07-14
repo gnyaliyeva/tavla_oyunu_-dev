50ce82fbec4dbb6fc422d9050d93283264c49b45
gnyaliyeva <gnyaliyeva94@gmail.com>
2019-07-15 00:06:27 +0300

    code has done.

--- app.py
+++ app.py
@@ -1,14 +1,45 @@
 checkers = dict()
-starGates = [5, 6, 7, 8, 17, 18, 19, 20] # define special gates
 
 # getting checkers and dice from user
 for i in range(0, 5):
     data = input("Enter the key and value separated by ':' => ")
     data = data.split(':')
     checkers[int(data[0])] = int(data[1])
+    print(checkers.items())
 
 dice1 = int(input("Enter first dice => "))
 dice2 = int(input("Enter second dice => "))
+
+starGates = [5, 6, 7, 8, 17, 18, 19, 20] # define special gates
+
+
+# for find total point of tuple
+# dict_c: tmp checkers, move: key+dice
+def find_result(dict_c, key, value, move):
+    # total: result point
+    total = 0
+    if move <= 24:  # moves are not bigger than 24
+        if value - 1 == 1:  # detect key state, if the stone is a gate or not
+            # when the stone is a gate, than detect is it special gate or not
+            if key in starGates:
+                total += -2
+            else:
+                total += -1
+        dict_c.update({key: value-1})
+        if move in dict_c:  # detect the move existing state
+            move_val = dict_c.get(move)  # getting the move's amount
+            if move_val == 1:  # detect if the value will be a gate or not
+                # when the move will be a gate, than detect is it special gate or not
+                if move in starGates:
+                    total += 2
+                else:
+                    total += 1
+                total += 1 # saving the stone against rival
+            dict_c.update({move: move_val + 1})  # set the first dice move on tmp checkers
+        else:
+            dict_c.update({move: 1})  # set the first dice move on tmp checkers
+
+        return total
 
 
 def find_moves(checkers, dice1, dice2):
